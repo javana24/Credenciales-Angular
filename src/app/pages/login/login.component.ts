6d319260588f44b8813a3923e4866b7e43054cb3
javana24 <javibamar06@gmail.com>
2025-10-30 17:06:20 +0100

    version terminada

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,11 @@ import { AuthService } from '../../core/services/auth.service';
 export class LoginComponent {
 
   formLogin;
+
   //Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)]
   constructor(private formSvc:FormBuilder,
-    private auth:AuthService
+    private auth:AuthService,
+    private router: Router
   ){
     this.formLogin = this.formSvc.group({
       'email':['', [Validators.required, Validators.email]],
@@ -22,25 +25,43 @@ export class LoginComponent {
     });
   }
 
-  onSubmit(){
-    console.log(this.formLogin.value);
-    this.auth.login(this.formLogin.value as any);
+  onSubmit() {
+    if (this.formLogin.invalid) {
+      this.formLogin.markAllAsTouched();
+      return;
+    }
+
+    const formValue = this.formLogin.value;
+
+    try {
+      this.auth.login({
+        email: formValue.email!,
+        password: formValue.password!
+      });
+
+      alert('Usuario logueado correctamente');
+
+      this.router.navigate(['/dashboard']);
+
+    } catch (error: any) {
+      alert(error.message || 'Error al hacer el login');
+    }
   }
 
   getError(control:string){
-       
+
     switch(control){
       case 'email':
-        if(this.formLogin.controls.email.errors!=null && 
+        if(this.formLogin.controls.email.errors!=null &&
            Object.keys(this.formLogin.controls.email.errors).includes('required'))
            return "El campo email es requerido";
-        else if(this.formLogin.controls.email.errors!=null && 
+        else if(this.formLogin.controls.email.errors!=null &&
            Object.keys(this.formLogin.controls.email.errors).includes('email'))
            return "El email no es correcto";
-        
+
         break;
-      case 'password': 
-        if(this.formLogin.controls.password.errors!=null && 
+      case 'password':
+        if(this.formLogin.controls.password.errors!=null &&
            Object.keys(this.formLogin.controls.password.errors).includes('required'))
            return "El campo email es requerido";
         break;
